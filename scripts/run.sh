dc34690d6b573d0f8169e72a4310ddf10f446ae8
Rakshit Menpara <deltasquare4@gmail.com>
2018-10-18 22:43:47 +0530

    Add IP address check into VPN

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
 # Install Wireguard. This has to be done dynamically since the kernel
 # module depends on the host kernel version.
@@ -21,14 +21,30 @@ interface=`echo $interfaces | head -n 1`
 echo "$(date): Starting Wireguard"
 wg-quick up $interface
 
+VPN_IP=$(grep -Po 'Endpoint\s=\s\K[^:]*' $interfaces)
+
 # Handle shutdown behavior
-finish () {
+function finish () {
     echo "$(date): Shutting down Wireguard"
     wg-quick down $interface
     exit 0
 }
 
+# Our IP address should be the VPN endpoint for the duration of the
+# container, so this function will give us a true or false if our IP is
+# actually the same as the VPN's
+function has_vpn_ip {
+    curl --silent --show-error --retry 10 --fail http://checkip.dyndns.com/ | \
+        grep $VPN_IP
+}
+
+# If our container is terminated or interrupted, we'll be tidy and bring down
+# the vpn
 trap finish SIGTERM SIGINT SIGQUIT
 
-sleep infinity &
-wait $!
+# Every minute we check to our IP address
+while [[ has_vpn_ip ]]; do
+    sleep 60;
+done
+
+echo "$(date): VPN IP address not detected"
